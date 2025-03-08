acc86948f0a92a8aba620f655f9116dac936a957
LutzGebelman <yuraosipov2012@gmail.com>
2025-03-08 21:20:29 +0200

    Move all functions from .c to .h

--- logger.h
+++ logger.h
@@ -57,7 +57,110 @@ bool BENCHMARKING = false;
 int Logger(const char *restrict inFile, const char *restrict inFunc,
            const int onLine, const enum loglevel loglevel, ...);
 
+// Function that initializes the logger file and opens it
 int InitLogger(void);
+
+int Logger(const char *restrict inFile, const char *restrict inFunc,
+           const int onLine, const enum loglevel loglevel, ...) {
+
+    if (!LOG_FILE_PTR) InitLogger();
+    if (CURRENT_LOG_LEVEL_FILE < loglevel &&
+        CURRENT_LOG_LEVEL_CONSOLE < loglevel)
+        return 0;
+
+    if (loglevel == BENCH && (!BENCHMARKING || !BENCH_LOG_FILE_PTR)) return 0;
+
+    char messageString[MAX_MESSAGE_LENGTH] = "";
+
+    va_list argptr;
+
+    va_start(argptr);
+    const char *restrict format = va_arg(argptr, char *);
+
+    FILE *file =
+        BENCHMARKING && loglevel == BENCH ? BENCH_LOG_FILE_PTR : LOG_FILE_PTR;
+
+    // Pass all the arguments to the vsprinf, which will fill it into the
+    // message string. If argptr is 0, then it will just be ignored (hopefully)
+    int ret = vsnprintf(messageString, MAX_MESSAGE_LENGTH, format, argptr);
+    va_end(argptr);
+
+    if (true) {
+        errno = EOVERFLOW;
+        fprintf(stderr,
+                "%m: message is too long. Limit is %d, length was %d\n",
+                MAX_MESSAGE_LENGTH,
+                ret);
+        return errno;
+    }
+
+    struct timeval time = (struct timeval){ 0, 0 };
+    gettimeofday(&time, 0);
+    char formatedTime[64] = "";
+    sprintf(formatedTime,
+            "%.2ld-%.2ld-%.2ld.%ld",
+            (time.tv_sec / 3600 + 3) % 24,
+            time.tv_sec / 60 % 60,
+            time.tv_sec % 60,
+            time.tv_usec);
+    // Actually print to stderr
+    const char *restrict format_string = "[%s] %*s %s->%s:%d %s\n";
+
+    if (CURRENT_LOG_LEVEL_CONSOLE >= loglevel)
+        fprintf(stderr,
+                format_string,
+                loglvlToString[loglevel],
+                strlen(formatedTime) + 19 - strlen(loglvlToString[loglevel]),
+                formatedTime,
+                inFile,
+                inFunc,
+                onLine,
+                messageString);
+
+    fprintf(file,
+            format_string,
+            loglvlToStringNoColor[loglevel],
+            strlen(formatedTime) + 19 - strlen(loglvlToString[loglevel]),
+            formatedTime,
+            inFile,
+            inFunc,
+            onLine,
+            messageString);
+    return 0;
+}
+
+int InitLogger(void) {
+    const time_t t           = time(NULL);
+    const struct tm timedate = *localtime(&t);
+
+    struct stat statThingi;
+    stat("logs", &statThingi);
+
+    // If logs exists, but is not a directory
+    // remove it and set errno to ENOENT
+    // So the next condition gets triggered and creates a directory
+    if (statThingi.st_mode / 010000 == 010) {
+        remove("logs");
+        errno = ENOENT;
+    }
+
+    if (errno == ENOENT) {
+        mkdir("logs", 0755);
+        errno = 0;
+    }
+
+    sprintf(LOG_FILE_NAME,
+            "logs/asteroids-%d-%02d-%02d_%02d-%02d-%02d.log",
+            timedate.tm_year + 1900,
+            timedate.tm_mon + 1,
+            timedate.tm_mday,
+            timedate.tm_hour,
+            timedate.tm_min,
+            timedate.tm_sec);
+
+    LOG_FILE_PTR = fopen(LOG_FILE_NAME, "a");
+    return 0;
+}
 
 
 #endif // LOGGER_H_
