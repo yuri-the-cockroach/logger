6b9359e335e8caf79cae0124c187746bfe179167
LutzGebelman <yuraosipov2012@gmail.com>
2025-03-08 21:19:38 +0200

    Remove format variable

--- logger.h
+++ logger.h
@@ -51,12 +51,11 @@ enum loglevel CURRENT_LOG_LEVEL_FILE    = 6;
 char LOG_FILE_NAME[64];
 bool BENCHMARKING = false;
 
-#define LOG(logLevel, format, ...) Logger(__FILE__, __FUNCTION__, __LINE__, logLevel, format, __VA_ARGS__)
+#define LOG(logLevel, ...) \
+    Logger(__FILE__, __FUNCTION__, __LINE__, logLevel, __VA_ARGS__)
 
-
-void Logger(const char *restrict inFile, const char *restrict inFunc,
-            const int onLine, const enum loglevel loglevel,
-            const char *restrict format, ...);
+int Logger(const char *restrict inFile, const char *restrict inFunc,
+           const int onLine, const enum loglevel loglevel, ...);
 
 int InitLogger(void);
 
